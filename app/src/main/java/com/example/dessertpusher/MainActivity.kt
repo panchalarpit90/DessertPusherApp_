3ea41d9feceaef9d4c098c8bb2837d6324bb5006
panchalarpit90 <123809457+panchalarpit90@users.noreply.github.com>
2024-04-16 13:55:50 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/dessertpusher/MainActivity.kt
+++ app/src/main/java/com/example/dessertpusher/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
     private var dessertsSold = 0
     private lateinit var dessertTimer: DessertTimer
 
-    // Contains all the views
+
     private lateinit var binding: ActivityMainBinding
 
 
